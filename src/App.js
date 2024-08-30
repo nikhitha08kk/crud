e3140a58338e242b97252839c9cc7a7024d07eb0
nikhitha08kk <nikhithakakkadan07@gmail.com>
2024-08-30 22:56:57 +0530

    todoapp

--- src/App.js
+++ src/App.js
@@ -1,32 +1,69 @@
-
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handleAddUpdateToDo = () => {
+    if (editIndex !== null) {
+      // Update existing ToDo
+      const updatedToDos = toDos.map((item, index) => (index === editIndex ? toDo : item));
+      setToDos(updatedToDos);
+      setEditIndex(null);
+    } else {
+      // Add new ToDo
+      setToDos([...toDos, toDo]);
+    }
+    setToDo(''); // Clear input field
+  };
+
+  const handleEditToDo = (index) => {
+    setToDo(toDos[index]);
+    setEditIndex(index);
+  };
+
+  const handleDeleteToDo = (index) => {
+    const updatedToDos = toDos.filter((_, i) => i !== index);
+    setToDos(updatedToDos);
+  };
+
   return (
     <div className="app">
-     <div className="mainHeading">
+      <div className="mainHeading">
         <h1>ToDo List</h1>
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 
-        😄  
-         </h2>
+        <h2>Whoop, it's Wednesday 😄</h2>
       </div>
       <div className="input">
-        <input type="text" placeholder="🖊️ Add item..." />
-        <i className="fas fa-plus"></i>
+        <input
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)}
+          type="text"
+          placeholder="🖊️ Add item..."
+        />
+        <i onClick={handleAddUpdateToDo} className="fas fa-plus"></i>
+
       </div>
       <div className="todos">
-        <div className="todo">
-          <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>React tutorial</p>
-          </div>
-          <div className="right">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
+        {toDos.map((value, index) => {
+          return (
+            <div className="todo" key={index}>
+              <div className="left">
+                <input type="checkbox" name="" id="" />
+                <p>{value}</p>
+              </div>
+              <div className="right">
+              <i onClick={() => handleEditToDo(index)}   className="fas fa-edit"></i>
+              <i onClick={() => handleDeleteToDo(index)} className="fas fa-times"></i>
+               
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
